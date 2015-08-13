d79103bf2375be787ebf5c476c273c29d855b5da
Eric Chen <eric1599@gmail.com>
2015-08-13 01:35:29 -0700

    tests

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var url="https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=649e900ceb3111d0f58b376c3738da13&per_page=5&format=json&jsoncallback=JSON_CALLBACK";
+var url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=649e900ceb3111d0f58b376c3738da13&per_page=20&page=1&format=json&jsoncallback=JSON_CALLBACK";
 
 angular.module('myApp.view1', ['ngRoute'])
 
@@ -11,12 +11,31 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope','$http',function($scope, $http) {
-  $http.jsonp(url).
-    success(function(data){
-      $scope.photos=data.photos.photo;
-    }).
-    error(function() {
-//console.log('error');
-    });
-}]);
+.controller('View1Ctrl',function($scope, Flickr) {
+ $scope.flickr= new Flickr();
+})
+
+.factory('Flickr',function($http) {
+  var Flickr = function () {
+    this.items = [];
+    this.busy = false;
+    this.page = 0;
+  };
+
+  Flickr.prototype.nextPage = function () {
+    if (this.busy) return;
+    this.busy = true;
+
+    $http.jsonp(url).success(function (data) {
+      var items = data.photos.photo;
+      for (var i = 0; i < items.length; i++) {
+        this.items.push(items[u].data);
+      }
+      this.busy = false;
+    }.bind(this));
+
+  };
+  return Flickr;
+});
+
+
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=649e900ceb3111d0f58b376c3738da13&per_page=10&page=2&format=json&jsoncallback=JSON_CALLBACK";
+
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +11,13 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
-}]);
+.controller('View2Ctrl', function($scope,$http) {
+      $http.jsonp(url).
+          success(function(data){
+console.log(data);
+            $scope.photos=data.photos.photo;
+          }).
+          error(function() {
+//console.log('error');
+          });
+    });
